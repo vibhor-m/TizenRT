495bece70a6b89b1dbed35b0a4d50f66e7882010
Vibhor Mahajan <vibhor.m@samsung.com>
2022-01-22 23:58:32 +0530

    sample app for scan

--- apps/examples/hello/hello_main.c
+++ apps/examples/hello/hello_main.c
@@ -57,9 +57,172 @@
 #include <tinyara/config.h>
 #include <stdio.h>
 
-/****************************************************************************
- * hello_main
- ****************************************************************************/
+#include <errno.h>
+#include <sys/socket.h>
+#include <wifi_manager/wifi_manager.h>
+#include <stress_tool/st_perf.h>
+
+#define TAG "[HELLO_SERVER]"
+
+#define CT_LOG(tag, fmt, args...) \
+printf(tag "[T%d] " fmt "\t%s:%d\n", getpid(), ##args, __FUNCTION__, __LINE__)
+
+#define CT_LOGE(tag, fmt, args...) \
+printf(tag "[ERR][T%d] " fmt "\t%s:%d\n", getpid(), ##args, __FUNCTION__, __LINE__)
+
+static sem_t g_wm_sem;
+
+#define HELLO_TEST_SIGNAL              \
+	do {                            \
+		sem_post(&g_wm_sem);        \
+		CT_LOG(TAG, "send signal"); \
+	} while (0)
+
+#define HELLO_TEST_WAIT                \
+	do {                            \
+		CT_LOG(TAG, "wait signal"); \
+		sem_wait(&g_wm_sem);        \
+	} while (0)
+
+/*
+ * callbacks
+ */
+static void wm_cb_sta_connected(wifi_manager_cb_msg_s msg, void *arg);
+static void wm_cb_sta_disconnected(wifi_manager_cb_msg_s msg, void *arg);
+static void wm_cb_softap_sta_join(wifi_manager_cb_msg_s msg, void *arg);
+static void wm_cb_softap_sta_leave(wifi_manager_cb_msg_s msg, void *arg);
+static void wm_cb_scan_done(wifi_manager_cb_msg_s msg, void *arg);
+
+static wifi_manager_cb_s g_wifi_callbacks = {
+	wm_cb_sta_connected,
+	wm_cb_sta_disconnected,
+	wm_cb_softap_sta_join,
+	wm_cb_softap_sta_leave,
+	wm_cb_scan_done,
+};
+
+void wm_cb_sta_connected(wifi_manager_cb_msg_s msg, void *arg)
+{
+	CT_LOG(TAG, "--> res(%d)", msg.res);
+	CT_LOG(TAG, "bssid %02x:%02x:%02x:%02x:%02x:%02x",
+		   msg.bssid[0], msg.bssid[1],
+		   msg.bssid[2], msg.bssid[3],
+		   msg.bssid[4], msg.bssid[5]);
+	int conn = 0;
+	if (WIFI_MANAGER_SUCCESS == msg.res) {
+		conn = 0;
+	} else {
+		conn = 2;
+	}
+	HELLO_TEST_SIGNAL;
+}
+
+void wm_cb_sta_disconnected(wifi_manager_cb_msg_s msg, void *arg)
+{
+	CT_LOG(TAG, "--> res(%d) reason %d", msg.res, msg.reason);
+	HELLO_TEST_SIGNAL;
+}
+
+void wm_cb_softap_sta_join(wifi_manager_cb_msg_s msg, void *arg)
+{
+	CT_LOG(TAG, "--> res(%d)", msg.res);
+	CT_LOG(TAG, "bssid %02x:%02x:%02x:%02x:%02x:%02x",
+		   msg.bssid[0], msg.bssid[1],
+		   msg.bssid[2], msg.bssid[3],
+		   msg.bssid[4], msg.bssid[5]);
+	HELLO_TEST_SIGNAL;
+}
+
+void wm_cb_softap_sta_leave(wifi_manager_cb_msg_s msg, void *arg)
+{
+	CT_LOG(TAG, "--> res(%d) reason %d", msg.res, msg.reason);
+	HELLO_TEST_SIGNAL;
+}
+
+void wm_cb_scan_done(wifi_manager_cb_msg_s msg, void *arg)
+{
+	CT_LOG(TAG, "--> res(%d)", msg.res);
+	if (msg.res != WIFI_MANAGER_SUCCESS || msg.scanlist == NULL) {
+		HELLO_TEST_SIGNAL;
+		return;
+	}
+	//ct_print_scanlist(msg.scanlist);
+	HELLO_TEST_SIGNAL;
+}
+
+static int hello_run_procedure(void)
+{
+	int res = sem_init(&g_wm_sem, 0, 0);
+	if (res != 0) {
+		return -1;
+	}
+	
+	wifi_manager_result_e wres = WIFI_MANAGER_SUCCESS;
+
+	/* Initialise Wifi */
+	CT_LOG(TAG, "init wi-fi");
+	wres = wifi_manager_init(&g_wifi_callbacks);
+	if (wres != WIFI_MANAGER_SUCCESS) {
+		CT_LOGE(TAG, "fail to init %d", wres);
+		return -1;
+	}
+	
+	/*  Start softAP */
+	CT_LOG(TAG, "start softAP");
+	wifi_manager_softap_config_s softap_config;
+	wm_get_softapinfo(&softap_config, "tp1x", "12341234", 1);
+	wres = wifi_manager_set_mode(SOFTAP_MODE, &softap_config);
+	if (wres != WIFI_MANAGER_SUCCESS) {
+		CT_LOGE(TAG, "fail to start softap %d", wres);
+		return -1;
+	}
+
+	/*  wait join event */
+	CT_LOG(TAG, "wait join event");
+	HELLO_TEST_WAIT;
+
+	/*  scan in softAP mode */
+	CT_LOG(TAG, "scan in softAP mode");
+	wres = wifi_manager_scan_ap(NULL);
+	if (wres != WIFI_MANAGER_SUCCESS) {
+		CT_LOGE(TAG, "fail to scan %d", wres);
+		return -1;
+	}
+	/*  wait scan event */
+	CT_LOG(TAG, "wait scan done event");
+	HELLO_TEST_WAIT;
+	
+	/*  set STA */
+	CT_LOG(TAG, "start STA mode");
+	wres = wifi_manager_set_mode(STA_MODE, NULL);
+	if (wres != WIFI_MANAGER_SUCCESS) {
+		CT_LOGE(TAG, "start STA fail %d", wres);
+		return -1;
+	}
+	
+	/*  scan in STA mode */
+	CT_LOG(TAG, "scan in STA mode");
+	wres = wifi_manager_scan_ap(NULL);
+	if (wres != WIFI_MANAGER_SUCCESS) {
+		CT_LOGE(TAG, "fail to scan %d", wres);
+		return -1;
+	}
+	/*  wait scan event */
+	CT_LOG(TAG, "wait scan done event in STA mode");
+	HELLO_TEST_WAIT; 
+	
+	/* Deinitialise Wifi */
+	CT_LOG(TAG, "deinit wi-fi");
+	wres = wifi_manager_deinit();
+	if (wres != WIFI_MANAGER_SUCCESS) {
+		CT_LOGE(TAG, "fail to deinit %d", wres);
+		return -1;
+	}
+
+	sem_destroy(&g_wm_sem);
+	return 0;
+}
+
 
 #ifdef CONFIG_BUILD_KERNEL
 int main(int argc, FAR char *argv[])
@@ -67,6 +230,7 @@ int main(int argc, FAR char *argv[])
 int hello_main(int argc, char *argv[])
 #endif
 {
+	hello_run_procedure();
 	printf("Hello, World!!\n");
 	return 0;
 }
